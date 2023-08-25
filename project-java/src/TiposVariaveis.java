470101ae2015cdaa9665b41b4065b37f8f408bee
eziul <ziulprof@gmail.com>
2023-08-24 22:03:03 -0300

    Variaveis

--- project-java/src/TiposVariaveis.java
+++ project-java/src/TiposVariaveis.java
@@ -1,5 +1,10 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Jarvis!");
+         int idade = 36;
+         double salarioMinimo = 15000;
+
+         short numeroCurto = 1;
+         final int NUMBERAWAY = numeroCurto;
+         
     }
 }
